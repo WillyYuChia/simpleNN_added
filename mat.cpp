7cda71afe89a1786acede53953716eeca4bdb4c9
b06901143 <b06901143@linux3.csie.ntu.edu.tw>
2020-04-30 13:00:44 +0800

    m=10000 try and failed

--- mat.cpp
+++ mat.cpp
@@ -1,20 +1,33 @@
+
 // mat.cpp
 #include <iostream>
 #include <string>
 #include <random>
+#include <ctime>
 
 using namespace std;
 
+double sec(clock_t start, clock_t end) {
+    return (end-start)/1000.0;
+}
+
 int main() {
-	int m = 10;
+	int m = 1000;
+	clock_t start = clock();
+	string tab = "\t";
+
 	std::default_random_engine generator;
 	std::normal_distribution<double> distribution(0.0, 1.0);
 	double n = distribution(generator);
-	int** mat = [m][m];
+	double mat[m][m];
 	for (int i=0; i<m; i++) {
 		for (int j=0; j<m; j++) {
 			mat[i][j] = distribution(generator);
-			cout << mat[i][j];
+			//cout << i*m+j << ":\t" << mat[i][j] << endl;
 		}
 	}
+
+	clock_t end = clock();
+	double time = sec(start, end);
+	cout << "Time:" << tab << time;
 }
