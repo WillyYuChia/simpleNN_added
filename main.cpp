da376c52b289fb56c799ffd15a25f6cd38e8a82a
Willy Wu <willywu@Willy-Wu.local>
2020-05-03 19:37:36 +0800

    fixed main.cpp

--- main.cpp
+++ main.cpp
@@ -23,14 +23,16 @@ double sec(clock_t start, clock_t end) {
     return to_double(end-start)/to_double(CLOCKS_PER_SEC);
 }
 
-vector<vector<double>> expand(vector<vector<double>> M) {
+vector<vector<double>> padding(vector<vector<double>> M, int p) {
+    clock_t start = clock()
     for (int i=0; i<M[0].size(); i++) {
-        M[i].insert(M[i].begin(), 1, 0.0);
-        M[i].push_back(0.0);
+        M[i].insert(M[i].begin(), p, 0.0);
+        M[i].push_back(M[i].end(), p, 0.0);
     }
-    M.push_back(vector<double>(M[0].size(), 0.0));
-    M.insert(M.begin(), 1, vector<double>(M[0].size()));
-    
+    M.insert(M.end(), 2, vector<double>(M[0].size(), 0.0));
+    M.insert(M.begin(), 2, vector<double>(M[0].size(), 0.0));
+    clock_t over = clock();
+    cout << sec(start, over) << endl;
     return M;
 }
 
@@ -38,6 +40,7 @@ int main() {
 
     default_random_engine generator;
     normal_distribution<double> distribution(0.0, 1.0);
+    int pad = 1
     int dim = 300;
     vector<vector<double>> M;
     
@@ -48,8 +51,6 @@ int main() {
         M.push_back(temp);
     }
     
-    clock_t start = clock();
-    expand(M);
-    clock_t end = clock();
+    padding(M, pad);
     cout << sec(start, end) << endl;
 }
